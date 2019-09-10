f7bfab5bf35760571c16cecc5732e05fda38cc84
FISHER Lewis <lfisher@altranuk.local>
2019-09-10 09:58:39 +0100

    part 1 check

--- Email Search.py
+++ Email Search.py
@@ -1,1 +1,8 @@
-
+fileName = "sample.txt"
+with open(fileName) as file:
+    textData = file.read()
+counter = 0
+for i in range(len(textData)):
+    if textData[i: i + 13] == '@softwire.com':
+        counter = counter + 1
+print(counter)
