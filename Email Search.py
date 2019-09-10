8cd20b9f4016079a7421abae57f1f79c75da93e7
FISHER Lewis <lfisher@altranuk.local>
2019-09-10 11:03:28 +0100

    Using regex for search

--- Email Search.py
+++ Email Search.py
@@ -1,8 +1,15 @@
+import re
 fileName = "sample.txt"
 with open(fileName) as file:
     textData = file.read()
-counter = 0
-for i in range(len(textData)):
-    if textData[i: i + 13] == '@softwire.com':
-        counter = counter + 1
-print(counter)
+
+# counter = 0
+# for i in range(len(textData)):
+#     if textData[i: i + 13] == '@softwire.com':
+#         counter = counter + 1
+# print(counter)
+
+# matches = re.findall('\S*@softwire.com', textData)
+# for match in matches:
+#     print(match)
+
